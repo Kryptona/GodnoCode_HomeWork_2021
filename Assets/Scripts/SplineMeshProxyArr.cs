f7d0bab3c1bd6cb6047b37ea36517d53e083a6f9
Дарья <dasha.seredenko.1997@gmail.com>
2021-06-23 00:28:43 +0400

    добавлена труба

---
+++ Assets/Scripts/SplineMeshProxyArr.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Race
+{
+#if UNITY_EDITOR
+    [CustomEditor(typeof(SplineMeshProxyArr))]
+    public class SplineMeshProxyEditorArr : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (GUILayout.Button("Update"))
+            {
+                (target as SplineMeshProxyArr).UpdatePoints();
+            }
+        }
+    }
+#endif
+
+    [RequireComponent(typeof(SplineMesh.Spline))]
+    public class SplineMeshProxyArr : MonoBehaviour
+    {
+        [SerializeField] private RaceTrackCurved _curvedTrack;
+
+        [SerializeField] private CurvedTrackPoint[] _points;
+
+        public void UpdatePoints()
+        {
+            var spline = GetComponent<SplineMesh.Spline>();
+
+            for (var i = 0; i < _points.Length ; i++)
+            {
+                var n0 = spline.nodes[i];
+                n0.Position = _points[i].transform.position;
+                n0.Direction = _points[i].transform.position + _points[i].transform.forward * _points[i].GetLength();
+            }
+
+            var lastNode = spline.nodes.Last();
+            lastNode.Position = _points[0].transform.position;
+            lastNode.Direction = _points[0].transform.position + _points[0].transform.forward * _points[0].GetLength();
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Tracks/RaceTrackCircular.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tracks;
+using UnityEngine;
+
+namespace Race
+{
+    public class RaceTrackCircular : RaceTrack
+    {
+        public override float GetTrackLength()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Vector3 GetPosition(float distance)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Vector3 GetDirection(float distance)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
+
